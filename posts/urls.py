f048c530b8865f0c3218f4e35e321e685123ebb7
MohanadBadra <78423601+MohanadBadra@users.noreply.github.com>
2023-02-23 21:33:13 +0200

    Add files via upload

--- posts/urls.py
+++ posts/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.blog, name="blog"),
+    path('post/<int:post_id>', views.posts, name='posts')
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 def blog(request):
-    return render(request, 'blog.html')
+    posts = Post.objects.all()
+    return render(request, 'blog.html', {'posts':posts})
+
+def posts(request, post_id):
+    post = Post.objects.get(id=post_id)
+    return render(request, 'posts.html', {'posts':post})
